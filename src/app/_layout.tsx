d8694ea9ccb7efd09ef52a282655fdfef4cab8d9
Charlie Francis <git@charliefrancis.dev>
2026-03-24 19:31:16 +0000

    feat: splash screen waits for clerk to load

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,16 @@
 import { ClerkProvider, useAuth } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 
 import { layout } from "@/styles/layout";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 if (!publishableKey) throw new Error("Add your Clerk Publishable Key to the .env file");
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
@@ -19,17 +23,19 @@ export default function RootLayout() {
 function RootNavigator() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded)
-    return (
-      <View style={layout.centered}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
+  useEffect(() => {
+    console.log({ isLoaded, isSignedIn });
+    if (isLoaded) {
+      SplashScreen.hide();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) return null;
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Protected guard={!!isSignedIn}>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="(home)" />
       </Stack.Protected>
       <Stack.Protected guard={!isSignedIn}>
         <Stack.Screen name="(auth)" />
