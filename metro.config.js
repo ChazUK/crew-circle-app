b01f1ebe85901b479d92c59fd5b83d32440ddd28
Charlie Francis <git@charliefrancis.dev>
2026-04-12 11:36:18 +0100

    fix: simplifying import paths

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,13 @@ const { withUniwindConfig } = require("uniwind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add resolver for path aliases
+config.resolver.alias = {
+  "@": "./src",
+  "@convex/api": "./convex/_generated/api",
+  "@convex/server": "./convex/_generated/server",
+};
+
 const { withStorybook } = require("@storybook/react-native/metro/withStorybook");
 
 module.exports = withUniwindConfig(withStorybook(config), {
